90179bcec79db7c5e8a52c661667244cd56acf6b
andreirtc <ralphandreicastillo326@gmail.com>
2024-05-18 18:18:30 +0800

    Based from the UML class diagram, create class TV and define a method wherein I used a non-parametarized constructor to set default channel and volume level to an integer (1) and on as a boolean (False)

---
+++ TV.py
@@ -0,0 +1,11 @@
+# Pseudocode
+
+# 1. Create a class and define functions and methods for operating the TV
+
+class TV:
+    def __init__(self):
+        self.channel = 1
+        self.volume_level = 1
+        self.on = False
+        
+# 2. When the class or the blueprint of the TV is done, create another file named test_tv.py to test and produce the channel and volume level of tv 1 and tv 2
