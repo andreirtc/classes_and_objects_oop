0c3034c97f1c4278dd59c3fa440f0154665f3151
andreirtc <ralphandreicastillo326@gmail.com>
2024-05-18 19:08:25 +0800

    From TV.py import class TV, then create a function that will create two objects from class TV

--- test_TV.py
+++ test_TV.py
@@ -1,2 +1,7 @@
 # 2. When the class or the blueprint of the TV is done, create another file named test_tv.py to test and produce the channel and volume level of tv 1 and tv 2
 
+from TV import TV
+
+def test_TV():
+    tv1 = TV()
+    tv2 = TV()
