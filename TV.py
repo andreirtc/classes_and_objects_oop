d61a77dbf00ff032ef19c7447e61bc7eeaa254e0
andreirtc <ralphandreicastillo326@gmail.com>
2024-05-18 18:42:54 +0800

    Create two methods to set the channel up or down. The if statements ensures the channel will be incremented by (1) as long as it does not exceed to maximum range (120). On the other hand, it will only b e decremented by (1) as long as it does not go below the minimum range (1)

--- TV.py
+++ TV.py
@@ -27,6 +27,14 @@ class TV:
     def set_volume(self, volume_level):
         if 1 <= volume_level <= 7:
             self.volume_level = volume_level
+    
+    def channel_up(self):
+        if self.channel < 120:
+            self.channel += 1
+
+    def channel_down(self):
+        if self.channel > 1:
+            self.channel -= 1
 
 
 # 2. When the class or the blueprint of the TV is done, create another file named test_tv.py to test and produce the channel and volume level of tv 1 and tv 2
