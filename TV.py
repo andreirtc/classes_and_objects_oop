e65b3afc5cbc6f911df603f0a6ea59222f95b2f5
andreirtc <ralphandreicastillo326@gmail.com>
2024-05-18 18:21:00 +0800

    Using a default constructor, define a method for turning the TV on and off

--- TV.py
+++ TV.py
@@ -7,5 +7,12 @@ class TV:
         self.channel = 1
         self.volume_level = 1
         self.on = False
-        
+    
+    def turn_on(self):
+        self.on = True
+    
+    def turn_off(self):
+        self.on = False
+
+
 # 2. When the class or the blueprint of the TV is done, create another file named test_tv.py to test and produce the channel and volume level of tv 1 and tv 2
