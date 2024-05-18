8f4ab0728ada2ff53c4dff149e30275213575552
andreirtc <ralphandreicastillo326@gmail.com>
2024-05-18 18:31:13 +0800

    Create a method to get and set the channel, the if statement ensures that the channel is within the range of 1 to 120, if it is, self.channel = channel sets the channel attribute of the instance to the provided value.

--- TV.py
+++ TV.py
@@ -14,5 +14,12 @@ class TV:
     def turn_off(self):
         self.on = False
 
+    def get_channel(self):
+        return self.channel
+    
+    def set_channel(self, channel):
+        if 1 <= channel <= 120:
+            self.channel = channel
+
 
 # 2. When the class or the blueprint of the TV is done, create another file named test_tv.py to test and produce the channel and volume level of tv 1 and tv 2
