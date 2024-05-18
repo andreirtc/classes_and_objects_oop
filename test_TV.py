de669eb7c11be5e6685276243ac0adf49cc8ce6d
andreirtc <ralphandreicastillo326@gmail.com>
2024-05-18 19:14:11 +0800

    Fixed the tv1 and tv2 turn_on because I forgot the parenthesis ()

--- test_TV.py
+++ test_TV.py
@@ -6,10 +6,10 @@ def test_TV():
     tv1 = TV()
     tv2 = TV()
 
-    tv1.turn_on
+    tv1.turn_on()
     tv1.set_channel(30)
     tv1.set_volume(3)
 
-    tv2.turn_on
+    tv2.turn_on()
     tv2.set_channel(3)
     tv2.set_volume(2)
